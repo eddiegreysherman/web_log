291235d61180d727e4f1b9d9a9b8e2e023448a89
eddie.sherman@gmail.com <Gr3750n123>
2017-07-26 22:43:52 -0400

    webblog from the udemy course complete...

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, make_response
 
 from src.common.database import Database
+from src.models.blog import Blog
+from src.models.post import Post
 from src.models.user import User
 
 app = Flask(__name__)
 app.secret_key = "Georgio"
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
 
 @app.route('/')
 def home_template():
@@ -17,10 +23,6 @@ def login_template():
 @app.route('/register')
 def register_template():
     return render_template('register.html')
-
-@app.before_first_request
-def initialize_database():
-    Database.initialize()
 
 @app.route('/auth/login', methods=['POST'])
 def login_user():
@@ -55,6 +57,45 @@ def user_blogs(user_id=None):
 
     return render_template('user_blogs.html',
                            blogs=blogs, email=user.email)
+
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts()
+
+    return render_template('posts.html',
+                           posts=posts,
+                           blog_title=blog.title,
+                           blog_id=blog._id)
+
+@app.route('/blogs/new', methods=['POST', 'GET'])
+def create_new_blog():
+    if request.method == 'GET':
+        return render_template('new_blog.html')
+    else:
+        title = request.form['title']
+        description = request.form['description']
+        user = User.get_by_email(session['email'])
+
+        new_blog = Blog(user.email, title, description, user._id)
+        new_blog.save_to_mongo()
+
+        return make_response(user_blogs(user._id))  # this redirects to the user_blogs method after creating
+
+@app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
+def create_new_post(blog_id):
+    if request.method == 'GET':
+        return render_template('new_post.html', blog_id=blog_id)
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post = Post(blog_id, title, content, user.email)
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))  # this redirects to the user_blogs method after creating
+
 
 if __name__ == '__main__':
     app.run(debug=True)
